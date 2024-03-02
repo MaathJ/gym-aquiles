138ad7542b6c2b796335a07e4a067a7d026eb066
Mathh <matiarevalo611@gmail.com>
2024-03-02 11:01:49 -0500

    Actualización Walter

--- membresia/D_servicio.php
+++ membresia/D_servicio.php
@@ -1,13 +1,31 @@
 <?php
 include('../config/dbconnect.php');
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['txt_id'])) {
+    $id = $_POST['txt_id'];
 
-$id=$_POST['txt_id'];
+    $sql = "DELETE FROM servicio WHERE id_se = $id";
 
-$sql="delete from servicio
-    where id_se = $id";
+    if(mysqli_query($cn, $sql)) {
+        $response = array(
+            'success' => true,
+            'message' => 'El servicio con ID ' . $id . ' ha sido eliminado exitosamente'
+        );
+    } else {
+        $response = array(
+            'success' => false,
+            'message' => 'Hubo un problema al eliminar el servicio'
+        );
+    }
 
-mysqli_query($cn,$sql);
+    echo json_encode($response);
+} else {
+    $response = array(
+        'success' => false,
+        'message' => 'No se recibió el ID del servicio'
+    );
 
-header('location: ../servicio.php');
+    echo json_encode($response);
+}
 ?>
+
--- tipo_rutina/D_tipo_rutina.php
+++ tipo_rutina/D_tipo_rutina.php
@@ -1,14 +1,30 @@
 <?php  
-
 include('../config/dbconnect.php');
 
-$codigo = $_GET['d'];
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['txt_id'])) {
+    $codigo = $_POST['txt_id'];
 
-$sql = "delete from tipo_rutina where id_tiru = '$codigo'";
+    $sql = "DELETE FROM tipo_rutina WHERE id_tiru = '$codigo'";
 
-mysqli_query($cn, $sql);
-	
-	header('location: ../tipo_rutina.php');
+    if (mysqli_query($cn, $sql)) {
+        $response = array(
+            'success' => true,
+            'message' => 'El tipo de rutina con el código ' . $codigo . ' ha sido eliminado correctamente.'
+        );
+    } else {
+        $response = array(
+            'success' => false,
+            'message' => 'Error al eliminar el tipo de rutina con el código ' . $codigo . ': ' . mysqli_error($cn)
+        );
+    }
 
+    mysqli_close($cn);
+} else {
+    $response = array(
+        'success' => false,
+        'message' => 'No se proporcionó un código para eliminar.'
+    );
+}
 
+echo json_encode($response);
 ?>
